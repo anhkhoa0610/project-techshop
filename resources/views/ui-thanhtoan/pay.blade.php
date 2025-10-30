ecc0d9280918a5398afa21558a8f4b2969a193f9
atkiett <authanhkietvta44@gmail.com>
2025-10-30 15:30:47 +0700

    COMMIT

--- resources/views/ui-thanhtoan/pay.blade.php
+++ resources/views/ui-thanhtoan/pay.blade.php
@@ -44,7 +44,7 @@
                                         value="{{ old('fullname', $user->full_name ?? '') }}" placeholder="Nguyễn Văn A"
                                         required>
                                 </div>
-                            
+
                                 <div>
                                     <label for="phone">Số điện thoại</label>
                                     <input id="phone" name="phone" type="tel"
@@ -65,7 +65,7 @@
 
                             <div style="margin-bottom:12px">
                                 <label for="address">Địa chỉ</label>
-                                <input id="address" type="text"  placeholder="Số nhà, tên đường" required>
+                                <input id="address" type="text" placeholder="Số nhà, tên đường" required>
                             </div>
                             <div class="two-col">
                                 <div>
