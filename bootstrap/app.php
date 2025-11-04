aa5e2cb309e1670d6884973bee583b91885ac715
nhutnguyen1198 <nm11nhut@gmail.com>
2025-11-04 13:54:48 +0700

    commit code

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,11 +13,21 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+
+        // Nhóm middleware cho API
+        $middleware->group('api', [
+            EnsureFrontendRequestsAreStateful::class, // Cho phép frontend gửi cookie / token hợp lệ
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\Auth\Middleware\Authenticate::class, // <- Quan trọng để dùng auth:sanctum
+        ]);
+
+        // Đặt alias cho middleware tùy chỉnh
         $middleware->alias([
-            'api.token' => \App\Http\Middleware\CheckApiToken::class,
             'checkrole' => \App\Http\Middleware\CheckRole::class,
+            'api.token' => \App\Http\Middleware\CheckApiToken::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
