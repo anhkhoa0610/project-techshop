00b8bd91997f402f3fb3f2870dc521e2c0fb274e
Lkiet123 <kietluong333@gmail.com>
2025-11-08 08:40:01 +0700

    commit ui-user-changePassword & update seeder

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -181,4 +181,39 @@ class UserController extends Controller
     {
         return view('ui-user.profile');
     }
+    //ui user changePassword
+    public function showChangePassword(){
+        return view('ui-user.change-password');
+    }
+
+    public function updatePassword(Request $request)
+{
+    $request->validate([
+        'current_password' => 'required',
+        'new_password' => [
+            'required',
+            'min:6',
+            'regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).{8,}$/',
+            'confirmed'
+        ],
+    ], [
+        'current_password.required' => 'Vui lòng nhập mật khẩu hiện tại.',
+        'new_password.required' => 'Vui lòng nhập mật khẩu mới.',
+        'new_password.min' => 'Mật khẩu mới phải có ít nhất 8 ký tự.',
+        'new_password.regex' => 'Mật khẩu mới phải chứa chữ hoa, chữ thường, số và ký tự đặc biệt.',
+        'new_password.confirmed' => 'Xác nhận mật khẩu mới không khớp.',
+    ]);
+
+    $user = auth()->user();
+
+    if (!\Hash::check($request->current_password, $user->password)) {
+        return back()->withErrors(['current_password' => 'Mật khẩu hiện tại không chính xác.']);
+    }
+
+    $user->update([
+        'password' => Hash::make($request->new_password),
+    ]);
+
+    return redirect()->route('user.profile')->with('success', 'Đổi mật khẩu thành công!');
+}
 }
--- resources/views/ui-user/profile.blade.php
+++ resources/views/ui-user/profile.blade.php
@@ -35,7 +35,7 @@
 
             <div class="profile-actions">
                 <a href="#" class="btn btn-edit">Chỉnh sửa</a>
-                <a href="#" class="btn btn-password">Thay mật khẩu</a>
+                <a href="{{ route('user.changePassword') }}" class="btn btn-password">Thay mật khẩu</a>
                 <!-- <form action="#" method="POST" onsubmit="return confirm('Bạn có chắc muốn xóa tài khoản này không?');">
                     @csrf
                     @method('DELETE')
