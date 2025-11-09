8dcab16bd47ca44fd19bf070d121e853d9ae3c4b
Lkiet123 <kietluong333@gmail.com>
2025-11-09 21:59:25 +0700

    commit ui-user-delete

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -31,7 +31,11 @@
 
 
 </body>
-<script src="{{ asset('js/index-script.js') }}"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-</html>
+<script src="{{ asset('js/index-script.js') }}"></script>
+
+@yield('scripts')
+
+</body>
