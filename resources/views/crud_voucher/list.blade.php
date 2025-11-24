d271f174b615fd2d42875fdfffca0ac4549e2018
nhutnguyen1198 <nm11nhut@gmail.com>
2025-11-24 15:16:56 +0700

    commit

--- resources/views/crud_voucher/list.blade.php
+++ resources/views/crud_voucher/list.blade.php
@@ -16,7 +16,7 @@
                 @csrf
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="editVoucherModalLabel">Chỉnh sửa Nhà Cung Cấp</h5>
+                        <h5 class="modal-title" id="editVoucherModalLabel">Chỉnh sửa Voucher</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
