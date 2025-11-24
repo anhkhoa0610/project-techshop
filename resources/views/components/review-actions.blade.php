ca49a6b5c33e47ebbecb26766285d44d0192e4ff
Lkiet123 <kietluong333@gmail.com>
2025-11-24 12:53:24 +0700

    commit crud_review(new)

---
+++ resources/views/components/review-actions.blade.php
@@ -0,0 +1,16 @@
+<div class="flex justify-center gap-2">
+    <a href="{{ route('reviews.edit', $review->review_id)}}" class="edit-btn p-2 rounded text-blue-600" title="Edit">
+
+        <i class="fa-regular fa-pen-to-square"></i>
+    </a>
+
+    <form id="delete-form-{{ $review->review_id}}" action="{{ route('reviews.destroy', $review->review_id) }}"
+        method="POST" style="display:inline;">
+        @csrf
+        @method('DELETE')
+        <button type="submit" class="btn btn-link p-0 m-0 align-baseline delete" title="Delete" data-toggle="tooltip"
+            >
+            <i class="material-icons text-danger">&#xE872;</i>
+        </button>
+    </form>
+</div>
